45c26f3195c4187c84120aa9fcb012b0ffeb9810
root@ubuntu <root@ubuntu.com>
2023-07-03 17:34:06 +0200

    first commit

---
+++ database/factories/LivreFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Livre>
+ */
+class LivreFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+		'title' => fake()->word(),
+		'description' => fake()->paragraph(),
+		'category_id' =>Category::all()->random()->id
+        ];
+    }
+}
